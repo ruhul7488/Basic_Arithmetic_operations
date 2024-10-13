8cec1aa971dbf108c58c3c60e6d1f82fc7d54369
Ruhul Amin <166214022+ruhul7488@users.noreply.github.com>
2024-10-13 23:40:30 +0530

    Update Find_Mean.java

--- Find_Mean.java
+++ Find_Mean.java
@@ -10,5 +10,17 @@
         // return result;
         System.out.println("mean of given set is:"+result);
     }
-    
+        public static void main(String[] ar){
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number of elements for calculate the mean of given set");
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("enter the elements");
+        for(int i=0;i<=n;i++){
+            arr[i]=sc.nextInt();
+        }
+       Mean m= new Mean();
+        m.findmean(arr);  
+    }
 }
